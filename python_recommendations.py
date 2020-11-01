0c58387dc8c0bd5fc4e7c3782272a1fed4536866
weronikazapedowska <73549761+weronikazapedowska@users.noreply.github.com>
2020-11-01 14:45:36 +0100

    Add files via upload

--- python_recommendations.py
+++ python_recommendations.py
@@ -1,40 +1,6 @@
 import pandas as pd
 
-#data is a dataframe
-data = pd.read_csv('userReviews.csv', sep=';')
-
-#the same as probably SELECT function in SQL
-print(data.head())
-print(data[:3])
-print(data.movieName.iloc[1])
-
-#create a new dataframe of reviews on my favorutie movie
-
-#insert all authors review in the datafram,e but only if the moives was called "beach-rats"
-subset = data[data.movieName == 'about-a-boy']
-
-#overview of the authors who reviewed the movies
-print(subset)
-
-#Create final dataframe for the recommendations that could also include the relative and final score{
-recommendations = pd.DataFrame(columns=data.columns.tolist()+['rel_inc','abs_inc'])
-
-#worked until now
-
-#looping over all the authors that watched the same favourite movie and then save every author who gave the ranking for favourite movie - ask Arta for the command
-for idx, Author in subset.iterrows():
-    author = Author[['Author']].iloc[0]
-    ranking = Author[['Metascore_w']].iloc[0]
-    filter1 = (data.Author==author)
-    filter2 = (data.Metascore_w>ranking)
-    possible_recommendations = data[filter1 & filter2]
-    print(possible_recommendations.head())
-    possible_recommendations.loc[:,'rel_inc'] = possible_recommendations.Metascore_w/ranking
-    recommendations = recommendations.append(possible_recommendations)
-    
-import pandas as pd
-
-#data is a dataframe
+#data as a dataframe
 data = pd.read_csv('userReviews.csv', sep=';')
 
 #the same as SELECT function in SQL
@@ -42,36 +8,44 @@ print(data.head())
 print(data[:3])
 print(data.movieName.iloc[1])
 
-
-#create an empty new dataframe
-#insert all authors review in the datafram and only if it was for the movie "beach-rats"
+#creating a subset of the data regarding the movie "about-a-boy"
 subset = data[data.movieName == 'about-a-boy']
 
-#overview of the authors whom review the movie
+#overview of the authors whom reviewed the movie
 print(subset)
 
 #Create final dataframe for the recommendations that also include the relative score
 recommendations = pd.DataFrame(columns=data.columns.tolist()+['rel_inc'])
 
-#loop over all Authors that watched the same favorit movie and then save each author who gave a ranking for the favorit movie
+#loop over all Authors that watched the same favorit movie and then save each author who gave a ranking for the favorite movie
 for idx, Author in subset.iterrows():
+
+#save the author and ranking who reviewed this specific favourite movie
     author = Author[['Author']].iloc[0]
     ranking = Author[['Metascore_w']].iloc[0]
+    
+#create a new possible recommendations
     filter1 = (data.Author==author)
     filter2 = (data.Metascore_w>ranking)
     possible_recommendations = data[filter1 & filter2]
-    print(possible_recommendations.head())
+    
+#calcuate the relative increase
     possible_recommendations.loc[:,'rel_inc'] = possible_recommendations.Metascore_w/ranking
+    
+#change recommendations to the new recommendations
     recommendations = recommendations.append(possible_recommendations)
     
-#
+#sort the recommendations where the highest values relative are on top of that
 recommendations = recommendations.sort_values(['rel_inc'], ascending=False)
 
-#
+#drop the duplicates to decrease somehow the siez eo ffht efile
 recommendations = recommendations.drop_duplicates(subset='movieName', keep="first")
 
+#check the outcome first - if these recommendations make sense
 print(recommendations.head(50))
-#
+
+#creating a file with the recommendations
 recommendations.head(50).to_csv("PY_recommendationsBasedOnMetascore.csv", sep=";", index=False)
 
+#show the recommendations reults
 print(recommendations.head(50))
